0c674fd9c7ff0168670bdeb811ee62a3fa283108
lukki15 <50550773+lukki15@users.noreply.github.com>
2024-10-04 22:59:41 +0000

    build: replace doctest with gtest

--- test/source/greeter.cpp
+++ test/source/greeter.cpp
@@ -1,21 +1,21 @@
-#include <doctest/doctest.h>
+#include <gtest/gtest.h>
 #include <greeter/greeter.h>
 #include <greeter/version.h>
 
 #include <string>
 
-TEST_CASE("Greeter") {
+TEST(Greeter, Hello) {
   using namespace greeter;
 
   Greeter greeter("Tests");
 
-  CHECK(greeter.greet(LanguageCode::EN) == "Hello, Tests!");
-  CHECK(greeter.greet(LanguageCode::DE) == "Hallo Tests!");
-  CHECK(greeter.greet(LanguageCode::ES) == "¡Hola Tests!");
-  CHECK(greeter.greet(LanguageCode::FR) == "Bonjour Tests!");
+  EXPECT_EQ(greeter.greet(LanguageCode::EN), "Hello, Tests!");
+  EXPECT_EQ(greeter.greet(LanguageCode::DE), "Hallo Tests!");
+  EXPECT_EQ(greeter.greet(LanguageCode::ES), "¡Hola Tests!");
+  EXPECT_EQ(greeter.greet(LanguageCode::FR), "Bonjour Tests!");
 }
 
-TEST_CASE("Greeter version") {
+TEST(Greeter, Version) {
   static_assert(std::string_view(GREETER_VERSION) == std::string_view("1.0"));
-  CHECK(std::string(GREETER_VERSION) == std::string("1.0"));
+  EXPECT_EQ(std::string(GREETER_VERSION), std::string("1.0"));
 }
